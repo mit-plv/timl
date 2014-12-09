17f5ee41b8f91f91302ad241426d3067ab166fc0
Peng Wang <wangp.thu@gmail.com>
2014-12-09 00:33:15 -0500

    Writing typing

--- LambdaO.v
+++ LambdaO.v
@@ -116,38 +116,45 @@ Module StringMap := Map.Make String_as_OT.
 Section LambdaO.
 
   Inductive var :=
-    | VBound : nat -> var
-    | VFree : string -> var
+    | Vbound : nat -> var
+    | Vfree : string -> var
   .
 
-  Coercion VBound : nat >-> var.
-  Coercion VFree : string >-> var.
+  Coercion Vbound : nat >-> var.
+  Coercion Vfree : string >-> var.
 
-  Inductive metric :=
-  | Mvar (x : var) (field : nat)
-  | M0 
-  | M1
-  | Mplus (n1 n2 : metric)
-  | Mminus (n1 n2 : metric)
-  | Mmult (n1 n2 : metric)
-  | Mdiv (n1 n2 : metric)
-  | Mlog (n : metric)
-  | Mexp (n : metric)
+  Inductive formula :=
+  | Fvar (x : var) (field : nat)
+  | F0 
+  | F1
+  | Fplus (n1 n2 : formula)
+  | Fminus (n1 n2 : formula)
+  | Fmult (n1 n2 : formula)
+  | Fdiv (n1 n2 : formula)
+  | Flog (n : formula)
+  | Fexp (n : formula)
   .
 
-  Infix "+" := Mplus : metric_scope.
-  Delimit Scope metric_scope with M.
-  Infix "-" := Mminus : M.
-  Infix "*" := Mmult : M.
-  Infix "/" := Mdiv : M.
-  Open Scope M.
+  Coercion var_to_formula (x : var) := Fvar x 0.
+
+  Infix "+" := Fplus : formula_scope.
+  Delimit Scope formula_scope with F.
+  Infix "-" := Fminus : F.
+  Infix "*" := Fmult : F.
+  Infix "/" := Fdiv : F.
+  Open Scope F.
 
   Inductive type :=
   | Tunit
   | Tprod (t1 t2 : type)
   | Tsum (t1 t2 : type)
   | Tlist (elm : type)
-  | Tarrow (t1 t2 : type) (cost result_size : metric).
+  | Tarrow (t1 : type) (cost : formula) (result_size : list formula) (t2 : type)
+  | Tvar (x : var)
+  | Tabs (t : type)
+  .
+
+  Coercion Tvar : var >-> type.
 
   Inductive expr :=
     | Evar (x : var)
@@ -155,11 +162,24 @@ Section LambdaO.
     | Eapp (f : expr) (arg : expr)
     | Eabs (t : type) (body : expr)
     | Elet (t : type) (def : expr) (main : expr)
-    | Eletrec (defs : list (type * expr)) (main : expr)
+    (* 'Definition' means the RHS of := in letrec.
+       Each definition must be a function, so there is an implicit quantifier on the RHS of :=. The LHS of := are also implicitly quantified. For example:
+       letrec x := \a. a * x (a - 1) with
+              y := \b. b * y (b - 1) in
+              x 10 + y 20
+       is written as 
+       letrec #0 * #2 (#0 - 1) with   (#2, #1, #0 -> x, y, a)
+              #0 * #1 (#0 - 1) in     (#2, #1, #0 -> x, y, b)
+              #1 10 + #0 10           (#1, #0 -> x, y)  
+       This must-be-function restriction is necessary for the type system to work 
+    *)
+    | Eletrec (defs : list (type * type * expr)) (main : expr)
     | Ematch (target : expr) (branches : list (constr * expr))
     | Etapp (e : expr) (t : type)
     | Etabs (body : expr)
   .
+
+  Definition letrec_entry := (type * type * expr)%type.
 
   Coercion Evar : var >-> expr.
   Coercion Econstr : constr >-> expr.
@@ -169,24 +189,38 @@ Section LambdaO.
     | OPconstr c : IsOpaque (Econstr c)
   .
 
-  Definition apply_many f args := fold_left Eapp args f.
+  Definition apply_many f args := fold_left (fun f x => match x with inl e => Eapp f e | inr t => Etapp f t end) args f.
+
+  Definition app_many f args := fold_left Eapp args f.
 
   Inductive IsValue : expr -> Prop :=
   | Vabs t e : IsValue (Eabs t e)
   | Vapp f args : 
       IsOpaque f ->
-      (forall a, In a args -> IsValue a) ->
+      (forall a, In (inl a) args -> IsValue a) ->
       IsValue (apply_many f args)
   .
   
+  Infix "<<" := compose (at level 40) : prog_scope.
+  Infix ">>" := (flip compose) (at level 40) : prog_scope.
+  Definition apply {A B} (f : A -> B) x := f x.
+  Infix "$" := apply (at level 85, right associativity) : prog_scope.
+  Open Scope prog_scope.
+  
+  Arguments snd {A B} _.
+
   Inductive context :=
     | CTempty
     | CTapp1 (f : context) (arg : expr)
     | CTapp2 (f : expr) (arg : context) : IsValue f -> context
     | CTlet (t : type) (def : context) (main : expr)
-    | CTletrec (defs : list (type * expr)) (main : context)
+    | CTletrec (defs : list letrec_entry) (main : context) (* Only evaluate main. All the definitions are already values, since that are all functions *)
     | CTmatch (target : context) (branches : list (constr * expr))
+    | CTtapp (f : context) (t : type)
   .
+
+  Import ListNotations.
+  Open Scope list_scope.
 
   Fixpoint plug (c : context) (e : expr) : expr :=
     match c with
@@ -196,9 +230,18 @@ Section LambdaO.
       | CTlet t def main => Elet t (plug def e) main
       | CTletrec defs main => Eletrec defs (plug main e)
       | CTmatch target branches => Ematch (plug target e) branches
+      | CTtapp f t => Etapp (plug f e) t
     end.
 
-  Definition subst (e : expr) (v : expr) : expr.
+  Definition subst (v : expr) (e : expr) : expr.
+    admit.
+  Defined.
+
+  Definition subst_t_t (t2 : type) (t : type) : type.
+    admit.
+  Defined.
+
+  Definition subst_t (t : type) (e : expr) : expr.
     admit.
   Defined.
 
@@ -227,28 +270,29 @@ Section LambdaO.
       
   Definition Pair e1 e2 := Eapp (Eapp Cpair e1) e2.
 
-  Definition subst_many e values := fold_left subst values e.
+  Definition subst_many e values := fold_left (flip subst) values e.
 
   Inductive step : expr -> expr -> Prop :=
     | STcontext c e1 e2 : step e1 e2 -> step (plug c e1) (plug c e2)
-    | STbeta t body arg : IsValue arg -> step (Eapp (Eabs t body) arg) (subst body arg)
-    | STlet t v main : IsValue v -> step (Elet t v main) (subst main v)
+    | STapp t body arg : IsValue arg -> step (Eapp (Eabs t body) arg) (subst arg body)
+    | STlet t v main : IsValue v -> step (Elet t v main) (subst v main)
     | STletrec_instantiate defs c (n : nat) t e : 
         find n defs = Some (t, e) -> 
-        step (Eletrec defs (plug c (Evar n))) (Eletrec defs (plug c e)) 
+        step (Eletrec defs (plug c (Evar n))) (Eletrec defs (plug c e))  (* the definitions are only simplified, but not making any recursive or mutual-recursive call. All these calls are made only in the evaluation of 'main' *)
     | STletrec_finish defs v : IsValue v -> step (Eletrec defs v) v
     | STmatch_pair (c : constr) values branches e : 
         let n := arity c in
         find c branches = Some e -> 
         length values = n ->
         Forall IsValue values ->
-        step (Ematch (apply_many c values) branches) (subst_many e values)
+        step (Ematch (app_many c values) branches) (subst_many e values)
+    | STtapp body t : step (Etapp (Etabs body) t) (subst_t t body)
   .
 
   (* typing context *)
   Inductive tc_entry := 
-    | TEtype (t : type)
-    | TEtype_var.
+    | TEtyping (t : type)
+    | TEtypevar.
 
   Definition tcontext := StringMap.t tc_entry.
 
@@ -256,14 +300,95 @@ Section LambdaO.
     {
       find k c := StringMap.find k c
     }.
-      
-  Inductive typing : tcontext -> expr -> type -> metric -> metric -> Prop :=
-  | TPvar T (x : string) t : find x T = Some t -> typing T x t M0 x.
-  | TPapp : 
-      typing T e1 (Tarrow t1 t2 f g) n1 s1 ->
+
+  Fixpoint dim (t : type) : nat :=
+    match t with
+      | Tunit => 0
+      | Tprod t1 t2 => dim t1 + dim t2
+      | Tsum t1 t2 => dim t1 + dim t2
+      | Tlist t => 1 + dim t
+      | Tarrow _ _ _ _ => 0
+      | Tvar _ => 1
+      | Tabs _ => 0
+    end.
+
+  Fixpoint range begin len :=
+    match len with
+      | 0 => nil
+      | S n => begin :: range (S begin) n
+    end.
+
+  Definition size_vars (x : string) (t : type) := map (Fvar x) (range 0 (dim t)).
+
+  Definition subst_f (s : list formula) (f : formula) : formula.
+    admit.
+  Defined.
+
+  Definition subst_f_t (s : list formula) (t : type) : type.
+    admit.
+  Defined.
+
+  Definition substx_f (x : string) (s : list formula) (f : formula) : formula.
+    admit.
+  Defined.
+
+  Definition abs (x : string) (e : expr) : expr.
+    admit.
+  Defined.
+
+  Definition abs_f (x : string) (f : formula) : formula.
+    admit.
+  Defined.
+
+  Definition abs_t (x : string) (t : type) : type.
+    admit.
+  Defined.
+
+  Notation "# n" := (Vbound n) (at level 0).
+  Infix "@" := Fvar (at level 10).
+
+  Fixpoint repeat A (a : A) n :=
+    match n with
+      | 0 => nil
+      | S n => a :: repeat a n
+    end.
+
+  Definition ones := repeat F1.
+
+  Definition add_many A ls m := fold_left (fun m p => @StringMap.add A (fst p) (snd p) m) ls m.
+
+  Arguments fst {A B} _.
+
+  Inductive typing : tcontext -> expr -> type -> formula -> list formula -> Prop :=
+  | TPvar T (x : string) t : find x T = Some (TEtyping t) -> typing T x t F1 (size_vars x t)
+  | TPpair T : typing T Cpair (Tabs $ Tabs $ Tarrow #1 F1 [] $ Tarrow #1 F1 [#1@0; #0@0] $ Tprod #3 #2) F0 []
+  | TPapp T e1 e2 t1 t2 f g n1 n2 s1 s2 : 
+      typing T e1 (Tarrow t1 f g t2) n1 s1 ->
       typing T e2 t2 n2 s2 ->
-      typing T (Eapp e1 e2) t2 (n1 + n2 + subst_m f s2) (subst_m g s2)
-  | TPabs :
-      typing T (Eabs 
-  | TPpair : typing T Cpair () M0 M1
+      typing T (Eapp e1 e2) (subst_f_t s2 t2) (n1 + n2 + subst_f s2 f)%F (map (subst_f s2) g)
+  | TPabs T x t1 e t2 n s:
+      ~ StringMap.In x T -> 
+      typing (StringMap.add x (TEtyping t1) T) e t2 n s ->
+      typing T (Eabs t1 (abs x e)) (Tarrow t1 (abs_f x n) (map (abs_f x) s) (abs_t x t2)) F1 []
+  | TPtapp T e t2 t n:
+      typing T e (Tabs t) n [] ->
+      let t' := subst_t_t t2 t in
+      typing T (Etapp e t2) t' n (ones $ dim t')
+  | TPtabs T X e t s:
+      ~ StringMap.In X T ->
+      typing (StringMap.add X TEtypevar T) e t F1 s ->
+      typing T (Etabs (abs X e)) (Tabs (abs_t X t)) F1 []
+  | TPlet T t1 e1 e2 t2 n1 n2 s1 s2 x:
+      typing T e1 t1 n1 s1 ->
+      ~ StringMap.In x T ->
+      typing (StringMap.add x (TEtyping t1) T) e2 t2 n2 s2 ->
+      typing T (Elet t1 e1 (abs x e2)) t2 (n1 + substx_f x s1 n2)%F (map (substx_f x s1) s2)
+  | TPletrec T (defs : list letrec_entry) main t n s xs :
+      length xs = length defs ->
+      NoDup xs ->
+      (forall x, In x xs -> ~ StringMap.In x T) ->
+      let T' := add_many (combine xs $ map (fst >> fst >> TEtyping) defs) T in
+      (forall lhs_ti rhs_ti ei, In (lhs_ti, rhs_ti, ei) defs -> typing T' (Eabs rhs_ti ei) lhs_ti F1 []) ->
+      typing T' main t n s ->
+      typing T (Eletrec defs main) t n s.
       
