ae5f27528b0f43af28e783feb4cdf60bcee2feeb
Peng Wang <wangp.thu@gmail.com>
2015-03-08 23:23:30 -0400

    Tweaked type soundness theorem

--- TypeSoundness.v
+++ TypeSoundness.v
@@ -12,47 +12,107 @@ Local Open Scope list_scope.
                     (for y,z\not\in FV(e))
  *)
 
-Inductive nsteps : expr -> nat -> expr -> Prop :=
-| Nsteps0 e : nsteps e 0 e
-| NstepsS e1 e2 n e3 : step e1 e2 -> nsteps e2 n e3 -> nsteps e1 (S n) e3
-.
-
 Inductive steps : expr -> expr -> Prop :=
 | Steps0 e : steps e e
 | StepsS e1 e2 e3 : step e1 e2 -> steps e2 e3 -> steps e1 e3
 .
 
-Definition get_size (e : expr) : size.
+Definition typingsim T e t := exists c s, typing T e t c s.
+
+Definition nostuck e := forall e', steps e e' -> IsValue e' \/ exists e'', step e' e''.
+
+Lemma sound_wrt_nostuck :
+  forall e t,
+    typingsim [] e t ->
+    nostuck e.
+Proof.
+  admit.
+Qed.
+
+Definition value_of v t := IsValue v /\ typingsim [] v t.
+
+Inductive nsteps : expr -> nat -> expr -> Prop :=
+| Nsteps0 e : nsteps e 0 e
+| NstepsS e1 e2 n e3 : step e1 e2 -> nsteps e2 n e3 -> nsteps e1 (S n) e3
+.
+
+Local Open Scope G.
+
+(* concrete size *)
+Inductive csize :=
+| CStt
+| CSinl (_ : csize)
+| CSinr (_ : csize)
+| CSinlinr (a b: csize)
+| CSpair (a b: csize)
+| CSfold (_ : csize)
+| CShide (_ : csize)
+.
+
+Definition leCS : csize -> csize -> Prop.
   admit.
 Defined.
 
-Definition typingsim T e t := exists c s, typing T e t c s.
+Instance Le_csize : Le csize :=
+  {
+    le := leCS
+  }.
 
-Local Open Scope G.
+Definition get_size (e : expr) : csize.
+  admit.
+Defined.
 
 Definition nat_of_cexpr (c : cexpr) : option nat.
   admit.
 Defined.
 
-Definition nostuck e := forall e', steps e e' -> IsValue e' \/ exists e'', step e' e''.
+Definition csize_to_size (s : csize) : size.
+  admit.
+Defined.
 
-Theorem type_soundness : forall t1 c s e t, 
-  typing [TEtyping (t1, None)] e t c s -> 
-  (forall v1,
-     IsValue v1 ->
-     typingsim [] v1 t1 ->
-     nostuck (subst v1 e)) /\
-  exists Ct s0, 
+Coercion csize_to_size : csize >-> size.
+
+Instance Subst_csize_cexpr : Subst csize cexpr :=
+  {
+    substn n v b := substn n (v : size) b
+  }.
+
+Instance Subst_csize_size : Subst csize size :=
+  {
+    substn n v b := substn n (v : size) b
+  }.
+
+(*
+Definition csize_of_size (s : size) : option csize.
+  admit.
+Defined.
+*)
+
+Definition bounded t1 e c (s : size) :=
+  exists (C : nat) (xi0 : csize), 
     forall v1,
-      IsValue v1 ->
-      typingsim [] v1 t1 ->
-      let s1 := get_size v1 in
-      (s0 <= s1) ->
+      value_of v1 t1 ->
+      let xi1 := get_size v1 in
+      xi0 <= xi1 ->
       forall v n, 
         IsValue v -> 
+        (* n is the actual running time *)
         nsteps (subst v1 e) n v ->
-        (exists c_s1, nat_of_cexpr (subst s1 c) = Some c_s1 /\ n <= Ct * c_s1) /\
-        get_size v <= subst s1 s.
+        (* n is bounded *)
+        (exists c_xi1, nat_of_cexpr (subst xi1 c) = Some c_xi1 /\ n <= C * c_xi1) 
+(*
+        /\
+        (* xi is the actual result size *)
+        let xi := get_size v in
+        (* xi is bounded *)
+        (exists s_xi1, csize_of_size (subst xi1 s) = Some s_xi1 /\ xi <= s_xi1)
+*)
+.
+
+Lemma sound_wrt_bounded :
+  forall t1 e t c s, 
+    typing [TEtyping (t1, None)] e t c s -> 
+    bounded t1 e c s.
 Proof.
   admit.
 Qed.
