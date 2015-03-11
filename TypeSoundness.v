aad10d84ac655e6b8513edb3e24cae9aea4d8fad
Peng Wang <wangpeng@csail.mit.edu>
2015-03-11 17:29:35 -0400

    Defining logical relation

--- TypeSoundness.v
+++ TypeSoundness.v
@@ -17,19 +17,19 @@ Inductive steps : expr -> expr -> Prop :=
 | StepsS e1 e2 e3 : step e1 e2 -> steps e2 e3 -> steps e1 e3
 .
 
-Definition typingsim T e t := exists c s, typing T e t c s.
+Definition typingsim T e τ := exists c s, typing T e τ c s.
 
 Definition nostuck e := forall e', steps e e' -> IsValue e' \/ exists e'', step e' e''.
 
 Lemma sound_wrt_nostuck :
-  forall e t,
-    typingsim [] e t ->
+  forall e τ,
+    typingsim [] e τ ->
     nostuck e.
 Proof.
   admit.
 Qed.
 
-Definition value_of v t := IsValue v /\ typingsim [] v t.
+Definition value_of v τ := IsValue v /\ typingsim [] v τ.
 
 Inductive nsteps : expr -> nat -> expr -> Prop :=
 | Nsteps0 e : nsteps e 0 e
@@ -43,7 +43,6 @@ Inductive csize :=
 | CStt
 | CSinl (_ : csize)
 | CSinr (_ : csize)
-| CSinlinr (a b: csize)
 | CSpair (a b: csize)
 | CSfold (_ : csize)
 | CShide (_ : csize)
@@ -66,7 +65,7 @@ Definition nat_of_cexpr (c : cexpr) : option nat.
   admit.
 Defined.
 
-Definition csize_to_size (s : csize) : size.
+Definition csize_to_size (ξ : csize) : size.
   admit.
 Defined.
 
@@ -82,24 +81,47 @@ Instance Subst_csize_size : Subst csize size :=
     substn n v b := substn n (v : size) b
   }.
 
-Definition bounded t1 e c (s : size) :=
-  exists (C : nat) (xi0 : csize), 
-    forall v1,
-      value_of v1 t1 ->
-      let xi1 := get_size v1 in
-      xi0 <= xi1 ->
+Notation "| v |" := (get_size v) (at level 10).
+
+(* An Logical Step-indexed Logical Relation (LSLR) for boundedness *)
+
+Inductive Rel :=
+
+Fixpoint V τ ξ ρ C θ {struct τ} : Rel :=
+  match τ, ξ, θ with
+    | Tvar α, _, _ => ρ α
+    | Tunit, _, _ => λ v ↓ τ, ⊤
+    | τ₁ × τ₂, Spair ξ₁ ξ₂, _ => λ v ↓ ρ τ, ∃ a b, v = Epair a b /\ a ∈ V τ₁ ξ₁ ρ /\ b ∈ V τ₂ ξ₂ ρ
+    | τ' + _, Sinl ξ, _ => λ v ↓ ρ τ, ∃ v', v = Einl v' /\ v' ∈ V τ' ξ ρ
+    | _ + τ', Sinr ξ, _ => λ v ↓ ρ τ, ∃ v', v = Einr v' /\ v' ∈ V τ' ξ ρ
+    | Tarrow τ₁ c s τ₂, _, THarrow θ₁ ξ₀ θ₂ => λ v ↓ ρ τ, ∀ ξ₁ (v₁ ∈ V τ₁ ξ₁ ρ C θ), ξ₀ ≤ |v₁| ⇒ Eapp v v' ∈ E τ₂ c s (add ξ₁ ρ) C θ₂
+    | Tuniversal c s τ, _, _ => λ v ↓ ρ τ, ∀ τ', ∀2 S : VSet τ', Etapp v τ' ∈ E τ c s (add (τ', S) ρ) C θ
+    | Trecur τ, Sfold ξ, _ => μ S, λ v ↓ ρ τ, ∃ v', v = Efold τ v' /\ ▹ (v' ∈ V τ ξ (add (ρ τ, S) ρ) C θ)
+    | _ _ _ => ⊥
+  end
+with 
+E τ c s ρ C θ :=
+  λ e : ρ τ, ∀ (v ∈ Val), ∀1 n, nsteps e n v ⇒ ⌈ natP (fun nc => n ≤ C * nc) (ρ c) ⌉ /\ ∃1 ξ, ξ ≤ ρ s /\ v ∈ V τ ξ ρ C θ
+.
+
+Definition bounded τ₁ e c (s : size) :=
+  exists (C : nat) (ξ₀ : csize), 
+    forall v₁,
+      value_of v₁ τ₁ ->
+      let ξ₁ := |v₁| in
+      ξ₀ <= ξ₁ ->
       forall v n, 
         IsValue v -> 
         (* n is the actual running time *)
-        nsteps (subst v1 e) n v ->
+        nsteps (subst v₁ e) n v ->
         (* n is bounded *)
-        (exists c_xi1, nat_of_cexpr (subst xi1 c) = Some c_xi1 /\ n <= C * c_xi1) 
+        (exists nc, nat_of_cexpr (subst ξ₁ c) = Some nc /\ n <= C * nc) 
 .
 
 Lemma sound_wrt_bounded :
-  forall t1 e t c s, 
-    typing [TEtyping (t1, None)] e t c s -> 
-    bounded t1 e c s.
+  forall τ₁ e τ c s, 
+    typing [TEtyping (τ₁, None)] e τ c s -> 
+    bounded τ₁ e c s.
 Proof.
   admit.
 Qed.
