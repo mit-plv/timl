51462450261615b8b6cac62019d60eccdae78ca4
Peng Wang <wangpeng@csail.mit.edu>
2016-10-10 16:22:08 -0400

    Proved interp_prop_eq_sym and interp_prop_eq_trans

--- proof/Safe.v
+++ proof/Safe.v
@@ -1380,17 +1380,6 @@ Module M (Time : TIME).
       lift2 ks f a (lift1 ks g b) = lift2 ks (fun x y => f x (g y)) a b.
     Admitted.
     
-  Fixpoint lift4 arg_ks : forall t1 t2 t3 t4 t, (t1 -> t2 -> t3 -> t4 -> t) -> interp_sorts arg_ks t1 -> interp_sorts arg_ks t2 -> interp_sorts arg_ks t3 -> interp_sorts arg_ks t4 -> interp_sorts arg_ks t :=
-    match arg_ks return forall t1 t2 t3 t4 t, (t1 -> t2 -> t3 -> t4 -> t) -> interp_sorts arg_ks t1 -> interp_sorts arg_ks t2 -> interp_sorts arg_ks t3 -> interp_sorts arg_ks t4 -> interp_sorts arg_ks t with
-    | [] =>
-      fun t1 t2 t3 t4 t f x1 x2 x3 x4 => f x1 x2 x3 x4
-    | arg_k :: arg_ks =>
-      fun t1 t2 t3 t4 t f x1 x2 x3 x4 => lift4 arg_ks (fun a1 a2 a3 a4 ak => f (a1 ak) (a2 ak) (a3 ak) (a4 ak)) x1 x2 x3 x4
-    end.
-
-    Lemma fuse_lift2_lift2_lift2 A B C D E F G ks (f : E -> F -> G) (g : A -> B -> E) (h : C -> D -> F) a b c d :
-      lift2 ks f (lift2 ks g a b) (lift2 ks h c d) = lift4 ks (fun a b c d  => f (g a b) (h c d)) a b c d.
-    Admitted.
     *)
 
     Lemma forall_all_ignore_premise ks P1 P2:
@@ -1401,6 +1390,13 @@ Module M (Time : TIME).
       lift1 ks f (lift2 ks g a b) = lift2 ks (fun a b => f (g a b)) a b.
     Admitted.
     
+  (* Lemma interp_prop_eq_refl' ks : forall A B (P : A -> B -> Prop), (forall a b, P a b) -> forall a b, forall_all ks (lift2 ks P a b). *)
+  (* Proof. *)
+  (*   induct ks; try rename a into k; intros A B P HP a b; cbn in *; eauto. *)
+  (*   rewrite fuse_lift1_lift2. *)
+  (*   eauto. *)
+  (* Qed. *)
+
   Lemma interp_prop_eq_refl' ks : forall A (P : A -> A -> Prop), (forall a, P a a) -> forall a, forall_all ks (lift2 ks P a a).
   Proof.
     induct ks; try rename a into k; intros A P HP a; cbn in *; eauto.
@@ -1414,30 +1410,100 @@ Module M (Time : TIME).
     cbn in *.
     intros i.
     eapply forall_all_ignore_premise.
-    eapply interp_prop_eq_refl'; eauto.
+    eapply interp_prop_eq_refl'.
+    eauto.
   Qed.
   
-  Lemma interp_prop_ground_truth L : forall p, interp_prop [] p -> interp_prop L p.
+  Fixpoint lift4 arg_ks : forall t1 t2 t3 t4 t, (t1 -> t2 -> t3 -> t4 -> t) -> interp_sorts arg_ks t1 -> interp_sorts arg_ks t2 -> interp_sorts arg_ks t3 -> interp_sorts arg_ks t4 -> interp_sorts arg_ks t :=
+    match arg_ks return forall t1 t2 t3 t4 t, (t1 -> t2 -> t3 -> t4 -> t) -> interp_sorts arg_ks t1 -> interp_sorts arg_ks t2 -> interp_sorts arg_ks t3 -> interp_sorts arg_ks t4 -> interp_sorts arg_ks t with
+    | [] =>
+      fun t1 t2 t3 t4 t f x1 x2 x3 x4 => f x1 x2 x3 x4
+    | arg_k :: arg_ks =>
+      fun t1 t2 t3 t4 t f x1 x2 x3 x4 => lift4 arg_ks (fun a1 a2 a3 a4 ak => f (a1 ak) (a2 ak) (a3 ak) (a4 ak)) x1 x2 x3 x4
+    end.
+
+    Lemma fuse_lift2_lift2_lift2 A B C D E F G ks (f : E -> F -> G) (g : A -> B -> E) (h : C -> D -> F) a b c d :
+      lift2 ks f (lift2 ks g a b) (lift2 ks h c d) = lift4 ks (fun a b c d  => f (g a b) (h c d)) a b c d.
+    Admitted.
+    
+    Lemma forall_all_same_premise ks P1 P2 P2':
+      forall_all ks (lift2 ks imply P1 P2) ->
+      forall_all ks (lift2 ks imply P1 (lift2 ks imply P2 P2')) ->
+      forall_all ks (lift2 ks imply P1 P2').
+    Admitted.
+    Lemma fuse_lift1_lift4 A B C D E F (f : E -> F) (g : A -> B -> C -> D -> E) ks a b c d :
+      lift1 ks f (lift4 ks g a b c d) = lift4 ks (fun a b c d => f (g a b c d)) a b c d.
+    Admitted.
+    
+  Lemma interp_prop_eq_sym' ks : forall A (P : A -> A -> A -> A -> Prop), (forall a b, P a b b a) -> forall a b, forall_all ks (lift4 ks P a b b a).
+  Proof.
+    induct ks; try rename a into k; intros A P HP a b; cbn in *; eauto.
+    rewrite fuse_lift1_lift4.
+    eauto.
+  Qed.
+
+  Lemma interp_prop_eq_sym L i i' :
+    interp_prop L (i == i')%idx ->
+    interp_prop L (i' == i)%idx.
   Proof.
     unfold interp_prop.
-    induct L; intros p Hp; cbn in *; eauto.
-    rename a into k.
-    eapply forall_all_imply_elim; eauto.
-    rewrite fuse_lift2_lift1.
+    intros H.
+    cbn in *.
+    eapply forall_all_same_premise; eauto.
+    eapply forall_all_ignore_premise.
     rewrite fuse_lift2_lift2_lift2.
-    (*here*)
+    simplify.
+    eapply interp_prop_eq_sym'.
+    eauto.
+  Qed.
+
+  Fixpoint lift6 arg_ks : forall t1 t2 t3 t4 t5 t6 t, (t1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t) -> interp_sorts arg_ks t1 -> interp_sorts arg_ks t2 -> interp_sorts arg_ks t3 -> interp_sorts arg_ks t4 -> interp_sorts arg_ks t5 -> interp_sorts arg_ks t6 -> interp_sorts arg_ks t :=
+    match arg_ks return forall t1 t2 t3 t4 t5 t6 t, (t1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t) -> interp_sorts arg_ks t1 -> interp_sorts arg_ks t2 -> interp_sorts arg_ks t3 -> interp_sorts arg_ks t4 -> interp_sorts arg_ks t5 -> interp_sorts arg_ks t6 -> interp_sorts arg_ks t with
+    | [] =>
+      fun t1 t2 t3 t4 t5 t6 t f x1 x2 x3 x4 x5 x6 => f x1 x2 x3 x4 x5 x6
+    | arg_k :: arg_ks =>
+      fun t1 t2 t3 t4 t5 t6 t f x1 x2 x3 x4 x5 x6 => lift6 arg_ks (fun a1 a2 a3 a4 a5 a6 ak => f (a1 ak) (a2 ak) (a3 ak) (a4 ak) (a5 ak) (a6 ak)) x1 x2 x3 x4 x5 x6
+    end.
+
+    Lemma fuse_lift4_lift2_lift2 A B C D E F G H I (i : A -> B -> G -> H -> I) (g : C -> D -> G) (h : E -> F -> H) ks a b c d e f :
+      lift4 ks i a b (lift2 ks g c d) (lift2 ks h e f) = lift6 ks (fun a b c d e f => i a b (g c d) (h e f)) a b c d e f.
+    Admitted.
+    
+    Lemma forall_all_same_premise_2 ks P1 P2 P2' P2'' :
+      forall_all ks (lift2 ks imply P1 P2) ->
+      forall_all ks (lift2 ks imply P1 P2') ->
+      forall_all ks (lift2 ks imply P1 (lift2 ks imply P2 (lift2 ks imply P2' P2''))) ->
+      forall_all ks (lift2 ks imply P1 P2'').
+    Admitted.
+    Lemma fuse_lift1_lift6 A B C D E F G H (h : G -> H) (g : A -> B -> C -> D -> E -> F -> G) ks a b c d e f :
+      lift1 ks h (lift6 ks g a b c d e f) = lift6 ks (fun a b c d e f => h (g a b c d e f)) a b c d e f.
+    Admitted.
+    
+  Lemma interp_prop_eq_trans' ks : forall A (P : A -> A -> A -> A -> A -> A -> Prop), (forall a b c, P a b b c a c) -> forall a b c, forall_all ks (lift6 ks P a b b c a c).
+  Proof.
+    induct ks; try rename a into k; intros A P HP a b c; cbn in *; eauto.
+    rewrite fuse_lift1_lift6.
+    eauto.
   Qed.
 
   Lemma interp_prop_eq_trans L a b c :
     interp_prop L (a == b)%idx ->
     interp_prop L (b == c)%idx ->
     interp_prop L (a == c)%idx.
-  Admitted.
-  Lemma interp_prop_eq_sym L i i' :
-    interp_prop L (i == i')%idx ->
-    interp_prop L (i' == i)%idx.
-  Admitted.
-
+  Proof.
+    unfold interp_prop.
+    intros Hab Hbc.
+    cbn in *.
+    eapply forall_all_same_premise_2; [eapply Hab | eapply Hbc |].
+    eapply forall_all_ignore_premise.
+    rewrite fuse_lift2_lift2_lift2.
+    rewrite fuse_lift4_lift2_lift2.
+    simplify.
+    eapply interp_prop_eq_trans'.
+    intros.
+    equality.
+  Qed.
+  
   Lemma interp_prop_le_refl L : forall i, interp_prop L (i <= i)%idx.
   Proof.
     unfold interp_prop.
