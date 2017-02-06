b6704c072b193399073f4d9c5ae883fb665681f6
Peng Wang <wangp.thu@gmail.com>
2017-02-06 14:51:16 +0800

    Proof: about to change [KArrow] from [sort -> kind -> kind] to [bsort -> kind -> kind]

--- proof/Soundness.v
+++ proof/Soundness.v
@@ -7753,8 +7753,6 @@ lift2 (fst (strip_subsets L))
 
   Hint Constructors tyeq.
   
-  (* values for denotational semantics *)
-
   Fixpoint interp_sorts L : interp_bsorts (map get_bsort L) Type -> Type :=
     match L with
     | [] => fun R => R
@@ -7797,26 +7795,25 @@ lift2 (fst (strip_subsets L))
     reflexivity.
   Qed.
                
-  Fixpoint interp_ty ty :=
-    match ty with
-    | TVar x => _
-    | TConst cn => cn
-    | TUnOp opr t => lift1 (fun t => TUnOp opr t) (interp_ty t ss)
-    | TBinOp (opr : ty_bin_op) (c1 c2 : ty)
-    | TArrow (t1 : ty) (i : idx) (t2 : ty)
-    | TAbs s t => interp_ty (s :: ss) t
-    | TApp t s i => lift2 app (interp_ty (s :: ss) t s_ret) (interp_idx_full i ss s)
-    | TQuan (q : quan) (k : kind) (t : ty)
-    | TQuanI (q : quan) (s : sort) (t : ty)
-    | TRec (k : kind) (t : ty) (args : list idx)
+  (* values for denotational semantics *)
 
-  (*here*)
-  
   Inductive sortv :=
   | SVBaseSort (b : bsort)
-  | SVSubset (s : bsort) (p : bsort -> Prop)
+  | SVSubset (b : bsort) (p : interp_bsort b -> Prop)
   .
 
+  Inductive kindv :=
+  | KVType
+  | KVArrow (s : sortv) (k : kindv)
+  .
+
+  Inductive idxv :=
+  | IVUnit
+  | IVBool (b : bool)
+  | IVNat (n : nat)
+  | IVTimeFun (arity : nat) (v : time_fun arity)
+  .
+      
   Inductive tyv :=
   | TVConst (cn : ty_const)
   | TVUnOp (opr : ty_un_op) (c : tyv)
@@ -7825,6 +7822,76 @@ lift2 (fst (strip_subsets L))
   | TVQuan (q : quan) (k : kindv) (t : tyv)
   | TVQuanI (q : quan) (s : sortv) (t : tyv)
   | TVRec (k : kindv) (t : tyv) (args : list idxv)
+  (* | TVAbs (s : sortv) (t : tyv) *)
+  .
+
+  Fixpoint interp_k k :=
+    match k with
+    | KType => tyv
+    | KArrow s k =>
+      match s with
+      | SBaseSort b => interp_bsort b -> interp_k k
+      | SSubset b _ => ((interp_bsort b -> interp_k k) * Prop)%type
+      end
+    end.
+
+  (*here*)
+    
+  Fixpoint interp_ty ty ks k_ret : interp_kinds ks (interp_kind k_ret) :=
+    match ty with
+    | TVar x => interp_var x ks k_ret
+    | TConst cn => lift0 _ (TVConst cn)
+    | TUnOp opr c =>
+      let f x := convert_kind_value KType res_k (interp_iunop opr x) in
+      lift1 arg_ks f (interp_idx c arg_ks (iunop_arg_bsort opr))
+    | TUnOp opr t => lift1 _ (fun t => TVUnOp opr t) (interp_ty t L)
+    | IUnOp opr c =>
+      let f x := convert_sort_value (iunop_result_bsort opr) res_k (interp_iunop opr x) in
+      lift1 arg_ks f (interp_idx c arg_ks (iunop_arg_bsort opr))
+    | TBinOp (opr : ty_bin_op) (c1 c2 : ty)
+    | TArrow (t1 : ty) (i : idx) (t2 : ty)
+    | TAbs s t => interp_ty (s :: ss) t
+    | TApp t s i => lift2 app (interp_ty (s :: ss) t s_ret) (interp_idx_full i ss s)
+    | TQuan (q : quan) (k : kind) (t : ty)
+    | TQuanI (q : quan) (s : sort) (t : ty)
+    | TRec (k : kind) (t : ty) (args : list idx)
+
+  Fixpoint interp_idx c arg_ks res_k : interp_bsorts arg_ks (interp_bsort res_k) :=
+    match c with
+    (* | IVar x => interp_var res_k x arg_ks id *)
+    | IVar x => interp_var x arg_ks res_k
+    | IConst cn => interp_iconst cn arg_ks res_k 
+    | IUnOp opr c =>
+      let f x := convert_sort_value (iunop_result_bsort opr) res_k (interp_iunop opr x) in
+      lift1 arg_ks f (interp_idx c arg_ks (iunop_arg_bsort opr))
+    | IBinOp opr c1 c2 =>
+      let f x1 x2 := convert_sort_value (ibinop_result_bsort opr) res_k (interp_ibinop opr x1 x2) in
+      lift2 arg_ks f (interp_idx c1 arg_ks (ibinop_arg1_bsort opr)) (interp_idx c2 arg_ks (ibinop_arg2_bsort opr))
+    | IIte c c1 c2 =>
+      lift3 arg_ks ite (interp_idx c arg_ks BSBool) (interp_idx c1 arg_ks res_k) (interp_idx c2 arg_ks res_k)
+    | ITimeAbs c =>
+      match res_k return interp_bsorts arg_ks (interp_bsort res_k) with
+      | BSTimeFun (S n) =>
+        interp_idx c (BSNat :: arg_ks) (BSTimeFun n)
+      | res_k => lift0 arg_ks (sort_default_value res_k)
+      end
+    | ITimeApp n c1 c2 => 
+      let f x1 x2 := convert_sort_value (BSTimeFun n) res_k (x1 x2) in
+      lift2 arg_ks f (interp_idx c1 arg_ks (BSTimeFun (S n))) (interp_idx c2 arg_ks BSNat)
+  end.
+
+  Definition interp_sorts2 L R :=
+    let bs := map get_bsort L in
+    let ps := strip_subsets L in
+    let p := and_all ps in
+    let P := interp_p bs p in
+    interp_bsorts_dep bs P (apply_all (P -> R) P R).
+
+  (*here*)
+  
+  Inductive sortv :=
+  | SVBaseSort (b : bsort)
+  | SVSubset (s : bsort) (p : bsort -> Prop)
   .
 
   (* Substitute a 'substitution group' for all variables. *)
